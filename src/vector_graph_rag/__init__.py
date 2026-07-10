de2cde25e78791b485779d9f4297c74f7bce4bf3
Cheney Zhang <chen.zhang@zilliz.com>
2026-07-10 08:36:01 +0000

    Release version 0.1.5
    Signed-off-by: Cheney Zhang <chen.zhang@zilliz.com>

--- src/vector_graph_rag/__init__.py
+++ src/vector_graph_rag/__init__.py
@@ -18,7 +18,7 @@ from vector_graph_rag.rag import VectorGraphRAG, create_rag
 from vector_graph_rag.storage.embeddings import EmbeddingModel
 from vector_graph_rag.storage.milvus import MilvusStore
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 __all__ = [
     "Settings",
